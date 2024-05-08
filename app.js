ede709cf968c79c29d6232788b044aa01d0d8480
xsmaster <165388332+xsmaster@users.noreply.github.com>
2024-05-08 23:24:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+
+
+// let a = 10;
+// let b = 15;
+// let c = 20;
+// console.log(a + b - c);
+
+// let a = 16;
+// let b = 15;
+// if (a > b);
+
+
+// let a = Math.floor(Math.random() * 100 + 1);
+// console.log(a);
+
+
+let num = Math.floor(Math.random() * 90 + 10); 
+let a = num + 1;
+let b = num + 2;
+let c = num + 3;
+let d = num + 4;
+let e = num + 5;
+console.log(a + b + c + d + e + " - " + num);
+
+
+let x = Math.floor(Math.random() * 9 + 1) 
+let y = Math.floor(Math.random() * 9 + 1)
+let z = Math.floor(Math.random() * 9 + 1)
+let t = Math.floor(Math.random() * 9 + 1)
+let summ = 0;
+let str = ""
+summ = summ + x + y
+str = str + z + t
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,25 @@
+let userName = prompt("Ismingizni kiriting");
+let sureName = prompt("Familyangizni kiriting");
+let userAge = prompt("Yoshingizni kiriting");
+let userEmail = prompt("Emailni kiriting");
+let userPhone = prompt("Telefon nomerni kiriting");
+let user = prompt("Yoqtirgan Faningizni kiriting")
+let userlive = prompt("Yashash joyingizni kiriting");
+let n = alert("Javobingiz uchun raxmat");
+console.log(`Name: ${userName}
+ Surname: ${sureName}
+  Age: ${userAge}
+   Email: ${userEmail}
+    Phone: ${userPhone}
+     fan:${user}
+      live: ${userlive}
+     end:${n}`);
+
+
+ 
+
+
+
+
+
+
